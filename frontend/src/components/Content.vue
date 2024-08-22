685a712cf4be014a9bad71d3a316d94bf21bd05d
Escapist_H <923737723@qq.com>
2024-08-22 10:27:46 +0800

    也是用上vue-router啦

--- frontend/src/components/Content.vue
+++ frontend/src/components/Content.vue
@@ -2,20 +2,22 @@
 
 import { NLayout, NLayoutContent } from 'naive-ui'
 
+import { RouterView } from 'vue-router';
+
 </script>
 
 <template>
     <n-layout>
         <n-layout-content class="main-container">
-            This is Content part
+            This is Content part <br>
+            <router-view />
         </n-layout-content>
     </n-layout>
 </template>
 
 <style scoped>
-.main-container{
+.main-container {
     background-color: #c3d7df;
     min-height: 80vh;
 }
-
 </style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    Home在这里啦
+</template>
+
+<style scoped></style>
